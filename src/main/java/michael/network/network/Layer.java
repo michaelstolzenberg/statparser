10003991b1f8f9e614f799172a67eaf50e41c7f6
michael <michaelstolzenberg@gmail.com>
2017-06-12 16:54:40 +0200

    fix variables

--- src/main/java/michael/network/network/Layer.java
+++ src/main/java/michael/network/network/Layer.java
@@ -4,12 +4,21 @@ import michael.network.network.function.Function;
 import org.jblas.DoubleMatrix;
 
 public class Layer {
-        public final DoubleMatrix sum;
-        public final DoubleMatrix activation;
-        public final Function function;
-        public Layer(DoubleMatrix sum,DoubleMatrix activation,Function function){
-            this.sum = sum;
-            this.activation = activation;
-            this.function = function;
-        }
+    public final Function function;
+    public final DoubleMatrix weights;
+    public final DoubleMatrix bias;    
+    public DoubleMatrix sum;
+    public DoubleMatrix activation;
+    public DoubleMatrix delta;
+    public DoubleMatrix weightsChange;
+    
+    public Layer(Function function, DoubleMatrix weights, DoubleMatrix bias){
+        this.function = function;
+        this.weights = weights;
+        this.bias = bias;
+        this.sum = DoubleMatrix.zeros(1,1);
+        this.activation = DoubleMatrix.zeros(1,1);
+        this.delta = DoubleMatrix.zeros(1,1);
+        this.weightsChange = DoubleMatrix.zeros(1,1);
     }
+}
